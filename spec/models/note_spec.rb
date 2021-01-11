aab2fc6216d66cf3f2ee4e4a9d2f20164bb9f9e1
Tkmt-U <niseganr@yahoo.co.jp>
2021-01-11 23:29:21 +0900

    note_spec.rb

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,70 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  let(:user) { FactoryBot.create(:user) }
-  let(:project) { FactoryBot.create(:project, owner: user) }
-
-  it "is valid with a user, project, and message" do
-    note = Note.new(
-      message: "This is a sample note.",
-      user: user,
-      project: project,
+  # 検索文字列に一致するメモを返すこと
+  it "returns notes that match the search term" do
+    user = User.create(
+      first_name: "Joe",
+      last_name: "Tester",
+      email: "joetester@example.com",
+      password: "Password",
     )
-    expect(note).to be_valid
+    project = user.projects.create(
+      name: "Test Project",
+    )
+    note1 = project.notes.create(
+      message: "This is the first note.",
+      user: user
+    )
+    note2 = project.notes.create(
+      message: "This is the second note.",
+      user: user,
+    )
+    note3 = project.notes.create(
+      message: "First, preheat the oven.",
+      user: user,
+    )
+
+    expect(Note.search("first")).to include(note1, note3)
+    expect(Note.search("first")).not_to include(note2)
   end
 
-  it "is invalid without a message" do
-    note = Note.new(message: nil)
-    note.valid?
-    expect(note.errors[:message]).to include("can't be blank")
-  end
+  # 検索結果が1件も見つからなければ空のコレクションを返すこと
+  it "returns an empty collection when no results are found" do
+    user = User.create(
+      first_name: "Joe",
+      last_name: "Tester",
+      email: "joetester@example.com",
+      password: "Password",
+    )
+    project = user.projects.create(
+      name: "Test Project",
+    )
+    note1 = project.notes.create(
+      message: "This is the first note.",
+      user: user,
+    )
+    note2 = project.notes.create(
+      message: "This is the second note.",
+      user: user,
+    )
+    note3 = project.notes.create(
+      message: "First, preheat the oven.",
+      user: user
+    )
 
-  it { is_expected.to have_attached_file(:attachment) }
-
-  describe "search message for a term" do
-    let!(:note1) {
-      FactoryBot.create(:note,
-        project: project,
-        user: user,
-        message: "This is the first note.",
-      )
-    }
-
-    let!(:note2) {
-      FactoryBot.create(:note,
-        project: project,
-        user: user,
-        message: "This is the second note.",
-      )
-    }
-
-    let!(:note3) {
-      FactoryBot.create(:note,
-        project: project,
-        user: user,
-        message: "First, preheat the oven.",
-      )
-    }
-
-    context "when a match is found" do
-      it "returns notes that match the search term" do
-        expect(Note.search("first")).to include(note1, note3)
-        expect(Note.search("first")).to_not include(note2)
-      end
-    end
-
-    context "when no match is found" do
-      it "returns an empty collection" do
-        expect(Note.search("message")).to be_empty
-        expect(Note.count).to eq 3
-      end
-    end
-  end
-
-  it "delegates name to the user who created it" do
-    user = instance_double("User", name: "Fake User")
-    note = Note.new
-    allow(note).to receive(:user).and_return(user)
-    expect(note.user_name).to eq "Fake User"
+    expect(Note.search("message")).to be_empty
   end
 end
