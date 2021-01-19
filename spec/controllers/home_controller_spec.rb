0ce34f467449e9fd7958f93cd639c3cedcab1242
Tkmt-U <niseganr@yahoo.co.jp>
2021-01-19 14:52:10 +0900

    update controllers/projects_controller_spec.rb

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe HomeController, type: :controller do
       get :index
       expect(response).to be_success
     end
+
+    # 200レスポンスを返すこと
+    it "returns a 200 response" do
+      get :index
+      expect(response).to have_http_status "200"
+    end
   end
 end
